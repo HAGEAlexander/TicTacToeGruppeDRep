6b3ed7792b0f9b763cb06a50e7599654474c72ef
HAGEAlexander <wi23b042@technikum-wien.at>
2024-06-01 16:06:45 +0200

    Update Main.java
    PrintBoard

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    static void printBoard()
+    {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | "
+                           + board[1] + " | " + board[2]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | "
+                           + board[4] + " | " + board[5]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | "
+                           + board[7] + " | " + board[8]
+                           + " |");
+        System.out.println("|---|---|---|");
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         board = new String[9];
@@ -12,6 +30,7 @@ public class Main {
         }
  
         System.out.println("Welcome to 3x3 Tic Tac Toe.");
+        printBoard();
         }
     }
 }
